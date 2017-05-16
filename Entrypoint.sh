979bdf7f7145d59a2c60076c6e6a170cf8f61758
root <ssmith@truepathtechnologies.com>
2017-05-16 14:29:46 -0400

    added advertise peers

--- Entrypoint.sh
+++ Entrypoint.sh
@@ -4,10 +4,19 @@
 # MASTER_ADDRESSES must be a comma delimited environment variable
 # HOSTGROUPS must be a comma delimted environment variable
 # SELF_HOSTNAME must be set
+# PEER_ADDRESSES must be set
+#
+# The big assumption around here is that proper DNS is set up and the scheduler 
+# will be able to handle setting the environment files upon deployment.
+# This should be fine because even if a poller is running with an outdated environment file
+# merlind will still know the new topology.
+# 
 #
 #############################################
 
 masters=(${MASTER_ADDRESSES//,/ })
+peers=(${PEER_ADDRESSES//,/})
+
 
 service_online(){
 	service sshd start
@@ -24,7 +33,7 @@ service_online(){
 }
 
 
-advertise(){
+advertise_masters(){
 	for i in "${!masters[@]}"
 	do
 		mon sshkey fetch ${masters[i]}  
@@ -35,9 +44,22 @@ advertise(){
 	done
 }
 
+advertise_peers(){
+        for i in "${!peers[@]}"
+        do
+                mon sshkey fetch ${peers[i]}
+                asmonitor mon sshkey fetch ${peers[i]}
+                mon node add ${peers[i]} type=peer
+                mon node ctrl ${peers[i]} mon node add ${SELF_HOSTNAME} type=peer
+                mon node ctrl ${peers[i]} mon restart
+        done
+
+}
 
 get_config(){
 	# Only getting config from one master because mon oconf always exits 0
+	# The fetch will initiate a restart of the the local merlind.
+	# This should be the only time we need to to restart locally since new pollers will restart us. 
 	mon oconf fetch ${masters[1]} 
 }
 
@@ -48,7 +70,8 @@ keep_swimming(){
 
 main(){
 	service_online
-	advertise
+	advertise_masters
+	advertise_peers
 	get_config
 	keep_swimming
 }
