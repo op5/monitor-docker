315769829707682da2b522a0d702d588553f47e2
Sebastian Smith <ssmith@autumn.genesee.om2.io>
2017-05-22 14:19:50 -0400

    added some debug for testing and created include folder

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM centos:6
 
-RUN yum -y install openssh-server \
+RUN yum -y install tmux multitail openssh-server \
 && curl http://repos.op5.com/tarballs/op5-monitor-7.3.11-20170428.tar.gz | tar -zx -C /tmp \
 && cd /tmp/op5-monitor-7.3.11 && ./install.sh --silent \
 && rm -rf /tmp/op5* \
@@ -11,8 +11,9 @@ RUN yum -y install openssh-server \
 EXPOSE 80 443 5666 15551 2222 162
 
 
-ADD ["ssh/","/tmp/ssh"] 
-ADD ["Entrypoint.sh","/Entrypoint.sh"]
+ADD ["include/ssh/","/tmp/ssh"] 
+ADD ["include/Entrypoint.sh","/Entrypoint.sh"]
+ADD ["include/tmux.conf","/etc/tmux.conf"]
 
 RUN cp -a /tmp/ssh/ /root/.ssh/ && chown -R root:root /root/.ssh/ && chmod -R 700 /root/.ssh && chmod 600 /root/.ssh/*
 RUN cp -a /tmp/ssh/ /opt/monitor/.ssh/ && chown -R monitor /opt/monitor/.ssh && chmod -R 700 /opt/monitor/.ssh && chmod 600 /opt/monitor/.ssh/*
--- include/Entrypoint.sh
+++ include/Entrypoint.sh
@@ -5,18 +5,24 @@
 # HOSTGROUPS must be a comma delimted environment variable
 # SELF_HOSTNAME must be set
 # PEER_ADDRESSES must be set
-#
-# The big assumption around here is that proper DNS is set up and the scheduler 
-# will be able to handle setting the environment files upon deployment.
-# This should be fine because even if a poller is running with an outdated environment file
-# merlind will still know the new topology.
-# 
+# DEBUG is a boolean 
 #
 #############################################
 
 masters=(${MASTER_ADDRESSES//,/ })
-peers=(${PEER_ADDRESSES//,/})
+peers=(${PEER_ADDRESSES//,/ })
 
+print(){
+    if [ $1 == "info" ];then 
+        echo -e '\033[37m' [INFO] $2 '\033[39;49m'
+    elif [ $1 == "warn" ];then
+        echo -e '\033[33m' [WARN] $2 '\033[39;49m'
+    elif [ $1 == "error" ]; then
+        echo -e '\033[31m' [ERROR] $2 '\033[39;49m'
+    elif [ $1 == "success" ]; then
+        echo -e '\033[32m' [SUCCESS] $2 '\033[39;49m'
+    fi 
+}
 
 service_online(){
 	service sshd start
@@ -40,7 +46,7 @@ advertise_masters(){
 		action=remove
 	fi
 
-	echo -e '\033[33m' Performing ${action} on Masters '\033[39;49m'
+    print "info" "Performing ${action} On Masters"
 
 	for i in "${!masters[@]}"
 	do
@@ -62,7 +68,7 @@ advertise_peers(){
 		action=remove
 	fi
 	
-	echo -e '\033[33m' Performing ${action} On Peers '\033[39;49m'	
+    print "info" "Performing ${action} On Peers"
 	
         for i in "${!peers[@]}"
         do
@@ -79,7 +85,7 @@ get_config(){
 	# Only getting config from one master because mon oconf always exits 0
 	# The fetch will initiate a restart of the the local merlind.
 	# This should be the only time we need to to restart locally since new pollers will restart us. 	
-	echo -e '\033[33m' Trying To Get Configuration From ${masters[0]} '\033[39;49m'
+    print "info" "Syncing Configuration With ${masters[0]}"
 	mon node ctrl ${masters[0]} asmonitor mon oconf push ${SELF_HOSTNAME}
 }
 
@@ -87,19 +93,62 @@ get_config(){
 shutdown(){
 	# If container is gracefully shutdown with SIGTERM (e.g. docker stop), remove
 	# pre-emptively remove itself
-	echo -e '\033[33m' Shutdown Initiated, Removing From Peer Lists '\033[39;49m'
-	advertise_peers remove
+	print "warn" "SIGTERM Caught! Removing From Cluster"
+    advertise_peers remove
 	advertise_masters remove
 }
 
 keep_swimming(){	
-	echo -e '\033[33m' Done '\033[39;49m'
-	tail -f /var/log/op5/merlin/daemon.log &	
-	wait $!
+    if [ $1 -ne 1 ];then
+        print "success" "Done"
+	    tail -f /var/log/op5/merlin/daemon.log &	
+	    wait $!
+    else
+        tmux new-session -d '/bin/bash' \; rename-window -t 0 SHELL \; new-window -d 'tail -f /var/log/messages'
+    fi
+}
+
+check_debug(){
+    if [ $1 -ne 1 ]; then
+        return 
+    else
+        print "warn" "DEBUG INFORMATION WILL BE DISPLAYED"
+        run_debug
+    fi
+}
+
+
+run_debug(){
+    if [ -z ${MASTER_ADDRESSES} ]; then
+        print "error" "No Master Addresses Are Set!"
+    else
+        print "success" "Master Addresses Are: ${MASTER_ADDRESSES}"
+    fi
+    if [ -z ${PEER_ADDRESSES} ]; then
+        print "warn" "No Peer Addresses Are Set!"
+    else
+        print "success" "Peer Addresses Are: ${PEER_ADDRESSES}"
+    fi
+    if [ -z ${SELF_HOSTNAME} ]; then
+        print "error" "Hostname Is Not Set!"
+    else
+        print "success" "My Hostname Is: ${SELF_HOSTNAME}"
+    fi
+    if [ -z ${HOSTGROUPS} ]; then
+        print "error" "I Am Not A Member Of Any Hostgroups!"
+    else
+        print "success" "My Hostgroups Are: ${HOSTGROUPS}"
+    fi
+
+ 
+
+#SET OP5 LOGLEVEL
+
 }
 
 
 main(){
+    check_debug ${DEBUG}
 	service_online
 	advertise_masters remove
 	advertise_masters add
