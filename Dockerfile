c72b6217ebb8a49a234f9b6ba3b52fe9b09e91fe
Ken D <kdobbins@op5.com>
2018-06-27 16:50:21 -0500

    combined function of multiple containers

--- Dockerfile
+++ Dockerfile
@@ -1,33 +1,20 @@
 FROM centos:6
 
 # Install EPEL first or else tmux and multitail wont be installed
-RUN yum -y install epel-release && yum -y install nc tmux multitail openssh-server \
-&& curl https://s3-eu-west-1.amazonaws.com/op5-filebase/Downloads/op5_monitor_archive/op5-monitor-7.3.18-20171114.tar.gz  | tar -zx -C /tmp \
-&& cd /tmp/op5-monitor-7.3.18 && ./install.sh --silent \
-&& rm -rf /tmp/op5* \
-&& yum -y install perl-IPC-Run \
-&& yum clean all
+COPY /entrypoint.d /usr/libexec/entrypoint.d/
 
+RUN yum -y install epel-release \
+    && mv /usr/libexec/entrypoint.d/repo/* /etc/yum.repos.d/ \
+    && mv /usr/libexec/entrypoint.d/repo-key/* /etc/pki/rpm-gpg/ \
+    && chmod 664 /etc/yum.repos.d/op5* \
+    && chmod 644 /etc/pki/rpm-gpg/*op5 \
+    && yum -y install wget nc tmux multitail openssh-server python-requests \
+    && yum groupinstall "op5 Monitor"
+    && yum clean all \
+    && sed -i 's/#Port 22/Port 2222/' /etc/ssh/sshd_config \
+    && chmod +x /usr/libexec/entrypoint.d/start.sh
 
 # OP5 Web UI, NRPE, Merlind, SSH, SNMPd 
-EXPOSE 80 443 5666 15551 2222 162
+EXPOSE 80 443 5666 15551 2222 161 162
 
-ADD ["start.sh","/start.sh"]
-ADD ["include/ssh_config","/tmp/ssh_config"]
-ADD ["include/Entrypoint.sh","/Entrypoint.sh"]
-ADD ["include/tmux.conf","/etc/tmux.conf"]
-ADD ["include/setup_interfaces","/setup_interfaces"]
-
-RUN mkdir -p /root/.ssh && chown -R root:root /root/.ssh/ && chmod -R 700 /root/.ssh
-RUN mkdir -p /opt/monitor/.ssh && chown -R monitor /opt/monitor/.ssh && chmod -R 700 /opt/monitor/.ssh
-RUN sed -i 's/#Port 22/Port 2222/' /etc/ssh/sshd_config
-RUN sed -i 's/#   StrictHostKeyChecking ask/   StrictHostKeyChecking no/' /etc/ssh/ssh_config
-RUN mkdir -p /etc/snmp && touch /etc/snmp/snmp.local.conf && echo "mibs +all" >> /etc/snmp/snmp.local.conf
-RUN cd /usr/share/snmp/mibs && chmod g+w . && chown root:apache .
-
-RUN chmod +x /setup_interfaces
-RUN chmod +x /Entrypoint.sh
-RUN chmod +x /start.sh
-
-CMD ["/start.sh"]
-#ENTRYPOINT ["/Entrypoint.sh"]
+CMD ["/usr/libexec/entrypoint.d/start.sh"]
---
+++ entrypoint.d/entrypoint.sh
@@ -0,0 +1,252 @@
+#!/bin/bash
+#############################################
+#
+# MASTER_ADDRESSES must be a comma delimited environment variable
+# HOSTGROUPS must be a comma delimted environment variable
+# SELF_HOSTNAME must be set
+# PEER_ADDRESSES must be set
+# DEBUG is a boolean, accepts 1 or true
+#
+#############################################
+
+# Create Array From Comma Delimited List
+masters=(${MASTER_ADDRESSES//,/ })
+peers=(${PEER_HOSTNAMES//,/ })
+
+# set default password to your set variable 'monitor' by default
+echo 'root:${ROOT_PASSWORD}' | chpasswd
+
+print(){
+    if [ $1 == "info" ];then 
+        echo -e '\033[36m' [INFO] $2 '\033[39;49m'
+    elif [ $1 == "warn" ];then
+        echo -e '\033[33m' [WARN] $2 '\033[39;49m'
+    elif [ $1 == "error" ]; then
+        echo -e '\033[31m' [ERROR] $2 '\033[39;49m'
+    elif [ $1 == "success" ]; then
+        echo -e '\033[32m' [SUCCESS] $2 '\033[39;49m'
+    fi 
+}
+
+trigger_hooks() {
+    echo "Triggering ${1} hooks"
+    /usr/libexec/entrypoint.d/hooks.py $1
+}
+
+import_backup() {
+    if [ ! -z "${IMPORT_BACKUP}" ]; then
+        file="/usr/libexec/entrypoint.d/backups/${IMPORT_BACKUP}"
+        if [ ! -e "$file" ]; then
+            echo -e "Error importing backup. Backup file ${file} does not exist."
+        else
+            echo -e "Backup file found. Importing: ${file} ..."
+    		op5-restore -n -b ${file}
+    		# remove all peer and poller nodes
+    		for node in `mon node list --type=peer,poller`; do mon node remove "$node"; done;
+    		mon stop
+        fi
+    fi
+}
+
+import_license() {
+    if [ ! -z "$LICENSE_KEY" ]; then
+    	file="/usr/libexec/entrypoint.d/licenses/${LICENSE_KEY}"
+    	if [ ! -e "$file" ]; then
+            echo -e "Error importing license. License file ${file} does not exist."
+    	else
+    		if [[ "$file" =~ \.lic$ ]]; then
+    			echo -e "License file found. Importing license file: ${file} ..."
+    			mv $file /etc/op5license/op5license.lic
+    			chown apache:apache /etc/op5license/op5license.lic
+    			chmod 664 /etc/op5license/op5license.lic
+    		else
+    			echo -e "Unable to import license file. License file extension must be .lic"
+    		fi
+    	fi
+    fi
+}
+
+service_online(){
+    service sshd start
+    service mysqld start
+    service merlind start
+    service naemon start
+    service httpd start
+    service nrpe start
+    service processor start
+    service rrdcached start
+    service synergy start
+    service smsd start
+    service collector start
+}
+
+advertise_masters(){
+    if [[ "${IS_POLLER}" =~ ^(yes|YES|Yes)$ ]]; then
+        for i in "${!masters[@]}"
+            do
+                if [ $1 == "add" ]; then
+                    print "info" "Performing Add On ${masters[i]}"
+                    mon sshkey fetch ${masters[i]}
+                    asmonitor mon sshkey fetch ${masters[i]}
+                    mon node add ${masters[i]} type=master
+                    mon node ctrl ${masters[i]} mon node add ${SELF_HOSTNAME} type=poller hostgroup=${HOSTGROUPS} takeover=no
+	                mon node ctrl  ${masters[i]} -- /usr/local/scripts/add_sync.sh ${SELF_HOSTNAME}
+                    mon node ctrl ${masters[i]} mon restart
+                else
+                    for node in `mon node list --type=master`
+                        do 
+                            print "info" "Performing Remove On $node"
+                            mon node remove "$node"
+                            mon node ctrl "$node" mon node remove ${SELF_HOSTNAME}
+                            mon node ctrl "$node" mon restart
+                        done
+                fi
+            done
+    else
+        echo -e "No Masters Added"
+}
+
+advertise_peers(){
+    if [[ "${IS_PEER}" =~ ^(yes|YES|Yes)$ ]]; then
+        if [ $1 == "add" ]; then
+            for i in "${!peers[@]}"
+                do
+                    nc -z ${peers[i]} 15551
+                        if [[ "$?" == "0" ]]; then
+                            print "info" "Performing Add On ${peers[i]}"
+                            mon sshkey fetch ${peers[i]}
+                            asmonitor mon sshkey fetch ${peers[i]}
+                            mon node add ${peers[i]} type=peer
+                            mon node ctrl ${peers[i]} mon node add ${SELF_HOSTNAME} type=peer
+                            mon node ctrl ${peers[i]} mon restart
+                        else
+                            print "error" "${peers[i]} not up, ignoring."
+                        fi
+                done
+         else
+	    	for node in `mon node list --type=peer`
+                do 
+                    print "info" "Performing Remove On $node"
+                    mon node remove "$node"
+                    mon node ctrl "$node" mon node remove ${SELF_HOSTNAME}
+                    mon node ctrl "$node" mon restart
+                done
+        fi
+    else
+        echo -e "No Masters Added"
+}
+
+get_config(){
+    # Only getting config from one master because mon oconf always exits 0
+    # The fetch will initiate a restart of the the local merlind.
+    # This should be the only time we need to to restart locally since new pollers will restart us.
+    print "info" "Syncing Configuration With ${masters[0]}"
+    mon node ctrl ${masters[0]} asmonitor mon oconf push ${SELF_HOSTNAME}
+}
+
+
+shutdown(){
+    # If container is gracefully shutdown with SIGTERM (e.g. docker stop), remove
+    # pre-emptively remove itself
+    print "warn" "SIGTERM Caught! Removing From Cluster"
+    kill ${!}; trigger_hooks poststop
+    advertise_peers remove
+    advertise_masters remove
+}
+
+
+keep_swimming(){
+    # This function should be the last thing to run. This is how the Container will
+    # persist. Under normal conditions we show a tail of merlins log and fork it 
+    # because this script needs to be PID 1 with NO CHILDREN due to the way parent
+    # processes handle SIGTERM                                                      
+    # The container must be run with -it for proper console access
+    
+    if [ "${debugging}" == "1" ]; then
+        read -n1 -r -p "Press Any Key To Enter The Debug Console..."
+        debug_console
+    else    
+        print "info" "Checking For Online Peers"
+        
+        advertise_peers add 
+        
+        print "info" "Getting Config From Master"
+        get_config
+
+        tail -f /var/log/op5/merlin/daemon.log &
+        wait $!
+        
+    fi
+    trigger_hooks poststart
+}
+
+debug_console(){
+    tmux new-session -d '/bin/bash' \; rename-window -t 0 Shell \; new-window -d 'multitail --mergeall /var/log/op5/merlin/daemon.log /var/log/op5/merlin/neb.log' \; rename-window -t 1 Merlind \; attach
+}
+
+check_debug(){
+    # This should be the first thing to run. Other functions that need to
+    # figure out if we are in debug mode should check if ${debugging} is 1
+    if [ "${DEBUG,,}" == "true" ] || [ "${DEBUG}" == "1" ]; then
+        debugging=1
+        print "warn" "DEBUG INFORMATION WILL BE DISPLAYED"
+        run_debug
+    else
+        return
+    fi
+}
+
+
+run_debug(){
+    # If debugging is 1, anything to run before the debug console
+    # should be placed here.
+    if [[ "${IS_POLLER}" =~ ^(yes|YES|Yes)$ ]]; then
+        if [ -z ${MASTER_ADDRESSES} ]; then
+            print "error" "No Master Addresses Are Set!"
+        else
+            print "success" "Master Addresses Are: ${MASTER_ADDRESSES}"
+        fi
+        if [ -z ${HOSTGROUPS} ]; then
+            print "error" "I Am Not A Member Of Any Hostgroups!"
+        else
+            print "success" "My Hostgroups Are: ${HOSTGROUPS}"
+        fi
+        if [ -z ${SELF_HOSTNAME} ]; then
+            print "error" "Hostname Is Not Set!"
+        else
+            print "success" "My Hostname Is: ${SELF_HOSTNAME}"
+        fi
+    fi
+    if [[ "${IS_PEER}" =~ ^(yes|YES|Yes)$ ]]; then
+        if [ -z ${PEER_ADDRESSES} ]; then
+            print "warn" "No Peer Addresses Are Set!"
+        else
+            print "success" "Peer Addresses Are: ${PEER_ADDRESSES}"
+        fi
+        if [ -z ${SELF_HOSTNAME} ]; then
+            print "error" "Hostname Is Not Set!"
+        else
+            print "success" "My Hostname Is: ${SELF_HOSTNAME}"
+        fi
+    fi
+    
+    # Change OP5 Log levels 
+    sed -i 's/level:.*/level: debug/' /etc/op5/log.yml
+    sed -i 's/log_level = info;/log_level = debug;/' /opt/monitor/op5/merlin/merlin.conf
+
+}
+
+
+main(){
+    check_debug
+    trigger_hooks prestart
+    import_backup
+    import_license
+    service_online
+    advertise_masters add
+    keep_swimming
+}
+
+# Graceful shutdown handling and run main()
+trap "shutdown" SIGTERM
+main
